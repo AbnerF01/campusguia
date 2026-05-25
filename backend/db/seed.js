096c0c2e23913fd9e8613175e2b638bdcab92c56
Abner <abnerandreeflores@hotmail.com>
2026-05-25 00:41:05 -0600

    soporte para base datos en Aiven

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -20,6 +20,7 @@ const { poIs } = require('./temp_places.js');
 async function seed() {
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'campusguia_db',
